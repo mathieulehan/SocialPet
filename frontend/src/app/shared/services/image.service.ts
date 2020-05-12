5cf76fe37ce2196be8bf0d20aa878783d32e0e90
mathieulehan <mathieu.lehan@gmail.com>
2020-05-12 13:50:03 +0200

    base 64 image decode w/ Angular

--- frontend/src/app/shared/services/image.service.ts
+++ frontend/src/app/shared/services/image.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  saveAnimal(animal: any): Observable<object> {
+    return this.http.post('/image', animal);
+  }
+
+  getAllImages(): Observable<object> {
+    return this.http.get('/images');
+  }
+
+  getRelatedImages(animal: any): Observable<object> {
+    return this.http.post('/perdu', animal);
+  }
+
 }
