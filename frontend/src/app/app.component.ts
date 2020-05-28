3b82355f47ba8736de473231232acff69b5edb0d
mathieulehan <mathieu.lehan@gmail.com>
2020-05-28 10:07:01 +0200

    retouches front 3

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,17 +10,6 @@ import {AuthService} from './shared/services/auth.service';
 export class AppComponent implements OnInit, OnDestroy {
   title = 'Social Pet';
   mobileQuery: MediaQueryList;
-  userIsConnected = false;
-
-  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
-
-  fillerContent = Array.from({length: 50}, () =>
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
-
   private readonly mobileQueryListener: () => void;
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private authService: AuthService) {
